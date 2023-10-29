131b992727beae6bb737b1a68b4c284e4ef13c16
TomSuworof <58502252+TomSuworof@users.noreply.github.com>
2023-10-29 13:30:33 +0000

    Deploying to javadoc from @ TomSuworof/resume@da272452cadef9562b054f664726c821d42dcae7 🚀

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.salat.resume","l":"Person"},{"p":"com.salat.resume","l":"TomSuworof"}];updateSearchResults();
